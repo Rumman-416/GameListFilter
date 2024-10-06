d0c949b00d7e0dc121c488e4476d2139f30911e0
Rumman <chowdhuryrumman78@gmail.com>
2024-10-06 22:34:10 +0530

    packages error solved

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from './App.jsx'
-import './index.css'
-import LayoutIndex from './layout/Index.jsx'
+import "./index.css";
+import LayoutIndex from "./layout/Index.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <LayoutIndex />
+      <LayoutIndex />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);

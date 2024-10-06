2cdb2bde4964ad34c7f1a4e6fac819c8491e2a58
Rumman <chowdhuryrumman78@gmail.com>
2024-10-06 22:37:00 +0530

    navbar after design solved

--- src/layout/components/Navbar.jsx
+++ src/layout/components/Navbar.jsx
@@ -5,47 +5,51 @@ const Navbar = () => {
   const location = useLocation();
   const [visibleContent, setVisibleContent] = useState("");
 
-  useEffect(()=>{
+  useEffect(() => {
     if (location.pathname === "/contact-us") {
       setVisibleContent("Contact");
     } else if (location.pathname === "/") {
       setVisibleContent("Video");
     }
-  },[location.pathname])
+  }, [location.pathname]);
 
-  const navHeading = [
-    {
-      label: "Video Games",
-      content: "Video",
-      link: "/",
-    },
-    {
-      label: "Contact",
-      content: "Contact",
-      link: "/contact-us",
-    },
-  ];
-
-  const showContent = (item) => {
-    setVisibleContent(item.content); 
+  const showContent = (content) => {
+    setVisibleContent(content);
   };
 
   return (
     <div className="p-10 md:p-14">
       <ul className="py-5 text-2xl font-montserrat font-medium flex flex-col gap-5 lg:flex-row lg:gap-20 ">
-        {navHeading.map((item, index) => (
-          <Link to={item.link} key={index} onClick={() => showContent(item)}>
-            <li
-              className={`text-[#fff] z-10 relative uppercase 
-                ${visibleContent === item.content ? 'after:content-["' + item.content + '"]' : 'after:content-[""]'} 
-                after:absolute after:px-4 after:-top-[25px] after:-left-[40px] 
-                after:text-5xl after:font-semibold after:text-text 
-                after:opacity-15`}
-            >
-              {item.label}
-            </li>
-          </Link>
-        ))}
+        <Link to="/" onClick={() => showContent("Video")}>
+          <li
+            className={`text-[#fff] z-10 relative uppercase 
+              ${
+                visibleContent === "Video"
+                  ? 'after:content-["Video"]'
+                  : 'after:content-[""]'
+              } 
+              after:absolute after:px-4 after:-top-[25px] after:-left-[40px] 
+              after:text-5xl after:font-semibold after:text-text 
+              after:opacity-15`}
+          >
+            Video Games
+          </li>
+        </Link>
+        <Link to="/contact-us" onClick={() => showContent("Contact")}>
+          <li
+            className={`text-[#fff] z-10 relative uppercase 
+              ${
+                visibleContent === "Contact"
+                  ? 'after:content-["Contact"]'
+                  : 'after:content-[""]'
+              } 
+              after:absolute after:px-4 after:-top-[25px] after:-left-[40px] 
+              after:text-5xl after:font-semibold after:text-text 
+              after:opacity-15`}
+          >
+            Contact
+          </li>
+        </Link>
       </ul>
     </div>
   );
